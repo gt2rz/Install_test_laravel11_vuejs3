df035f1b31a16a0493cd321b4bdd42ea0c6aaf80
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-04-24 16:18:18 -0500

    chore: Update npm dependency to latest stable version

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            input: ["resources/css/app.css", "resources/js/app.ts"],
             refresh: true,
         }),
         vue({
@@ -19,7 +21,11 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@/*': 'resources/js/spa_vue/*',
-        }
+            "@/*": "resources/js/spa_vue/*",
+        },
+    },
+    test: {
+        include: ["resources/js/**/*.spec.ts", "resources/js/**/*.test.ts"],
+        exclude: ["node_modules", "public", "resources/js/vendor", ],
     },
 });
