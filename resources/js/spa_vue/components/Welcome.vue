88eacbb9f84bcfdaf3eeff416e52f43ca4eb6c16
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-04-23 19:16:28 -0500

    chore: Update eslint and prettier configurations and dependencies

--- resources/js/spa_vue/components/Welcome.vue
+++ resources/js/spa_vue/components/Welcome.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
-    import { Greet } from '../types/Greet';
+import { ref } from "vue";
+import { Greet } from "../types/Greet";
 
-    interface Props {
-        title: string;
-        subtitle?: string;
-    }
+interface Props {
+    title: string;
+    subtitle?: string;
+}
 
-    const {title, subtitle} = defineProps<Props>();
+const { title, subtitle } = defineProps<Props>();
 
-    const greet = ref<Greet>({message: 'Hello World!'});
+const greet = ref<Greet>({ message: "Hello World!" });
 </script>
 
 <template>
     <div>
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
         <h1>Welcomerrr!!!!</h1>
         <h2>{{ title }}</h2>
         <h3 v-if="subtitle">{{ subtitle }}</h3>
